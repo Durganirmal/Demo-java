f41a9c9218e906421ea7661871b483c63a43608c
Durga Nirmal <durganirmal123@gmail.com>
2021-11-04 19:15:48 +0530

    Added :Addition Program

---
+++ src/demojava/Demo_Java.java
@@ -0,0 +1,13 @@
+package demojava;
+
+public class Demo_Java {
+    public static void main(String[] args);
+    int x=357;
+    int y=65;
+    int z=x+y;
+        System.out.println(z+ "Addition is");
+
+}
+
+
+    }
